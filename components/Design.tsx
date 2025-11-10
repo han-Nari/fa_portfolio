fbb39d48bcb35da153c9180b3e705d05df3dd7ce
lek <abungan082198@gmail.com>
2025-11-10 17:14:39 +0800

    updated

--- components/Design.tsx
+++ components/Design.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import Image from "next/image";
-
 export default function Design() {
   return (
     <>
       <header className="background-top relative">
-        <Image
-          className="absolute md:right-30 bottom-0 right-0 w-[150px] h-[150px] md:w-[200px] md:h-[200px]"
-          src={"/evernight.gif"}
-          alt="evernight"
-          width={300}
-          height={300}
-        />
         <h1 className="text-center title-header">
           Front end{" "}
           <span className="bg-linear-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
